bb4087500cc713d97cc8b173f0ccc6e4a1bba4cd
costadercole <costanza.dercole@student.uva.nl>
2025-02-26 16:53:42 +0100

    updated
    - added 10 conv layers with skip connections.

--- assignment_2/CNNs.py
+++ assignment_2/CNNs.py
@@ -11,24 +11,35 @@ class SimpleConvNet(pl.LightningModule):
         #######################
         # PUT YOUR CODE HERE  #
         #######################
-        self.layers = nn.Sequential(
-            # conv block 1
+        # Initial convolutional block: sets the foundation for skip connections
+        self.initial_conv = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
+            nn.MaxPool2d(2, 2)
+        )
 
-            # conv block 2
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2))
+        # List of convolutional blocks with increasing channels and skip connections
+        self.conv_blocks = nn.ModuleList()
+        in_channels = 16
+        out_channels = 32
 
+        # Creating 5 convolutional blocks with skip connections
+        for _ in range(5):
+            self.conv_blocks.append(nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2)
+            ))
+            in_channels = out_channels
+            out_channels *= 2 if out_channels < 128 else 128
+
+        # Classifier: reduces spatial dimensions and maps features to output
         self.classifier = nn.Sequential(
-            # linear layers
             nn.AdaptiveAvgPool2d(output_size=(4, 4)),
             nn.Flatten(),
-            nn.Linear(in_features=4 * 4 * 32, out_features=60),
+            nn.Linear(in_features=4 * 4 * 128, out_features=60),
             nn.ReLU(),
             nn.Linear(in_features=60, out_features=1)
         )
@@ -37,7 +48,17 @@ class SimpleConvNet(pl.LightningModule):
         #######################
 
     def forward(self, x):
-        x = self.layers(x)
+        # Pass input through initial convolution
+        x = self.initial_conv(x)
+        skip = x
+
+        # Pass through each convolutional block with skip connection
+        for block in self.conv_blocks:
+            x = block(x)
+            x += skip  # Adding skip connection to preserve information
+            skip = x
+
+        # Final classification layer
         x = self.classifier(x)
         return x
 
